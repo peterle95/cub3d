369b0b379d6e6a75576f833efc91fecef7da5d79
Peter Moelzer <moelzerpeter@gmail.com>
2025-01-19 14:26:27 +0100

    adding libft and minilibX

--- include/window.h
+++ include/window.h
@@ -28,6 +28,13 @@ typedef struct s_map
 	char	*f;
 	char	*c;
 	char	**map_ids;
+	char	**map_array;
+    int     map_height;
+    int     map_width;
+    int     player_x;
+    int     player_y;
+    char    player_dir;
+    int     elements_found;
 	int		id;
 	int		n;
 }	t_map;
@@ -59,5 +66,6 @@ int		set_trgb(int t, int r, int g, int b);
 void	add_random_pixels(t_data *data, int width, int height, int num_pixels);
 void	clear_image_to_colour(t_data *data, int colour);
 int		load_map_data(t_data *data, char *f_name);
-
+int		validate_map(t_data *data);
+int		error(char *message);
 #endif
